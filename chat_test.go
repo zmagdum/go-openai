e2dcb5f97de80ccc5736d29dacda875006d8d189
Zakir Magdum <zakir.magdum@device42.com>
2024-06-05 11:01:34 -0400

    updated the package name as it is not importing correctly

--- chat_test.go
+++ chat_test.go
@@ -12,9 +12,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/sashabaranov/go-openai"
-	"github.com/sashabaranov/go-openai/internal/test/checks"
-	"github.com/sashabaranov/go-openai/jsonschema"
+	"github.com/zmagdum/go-openai"
+	"github.com/zmagdum/go-openai/internal/test/checks"
+	"github.com/zmagdum/go-openai/jsonschema"
 )
 
 const (
--- completion_test.go
+++ completion_test.go
@@ -12,8 +12,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/sashabaranov/go-openai"
-	"github.com/sashabaranov/go-openai/internal/test/checks"
+	"github.com/zmagdum/go-openai"
+	"github.com/zmagdum/go-openai/internal/test/checks"
 )
 
 func TestCompletionsWrongModel(t *testing.T) {
